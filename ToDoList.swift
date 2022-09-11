fa5c8ea1beaba728df411f5fcba326e6618e7f53
Dhivyno <63943490+Dhivyno@users.noreply.github.com>
2022-09-11 10:22:32 +0800

    Create ToDoList.swift

---
+++ ToDoList.swift
@@ -0,0 +1,36 @@
+import SwiftUI
+
+func RandomPrompt() -> String{
+    let things = ["What is the apparatus needed to titrate hydrochloric acid and sodium hydroxide?"]
+    return String(things.randomElement()!)
+}
+
+struct ContentView: View {
+    @State var GoodThingPrompt: String = RandomPrompt()
+    @State var GoodThing2: String = ""
+    @State var GoodThing3: String = ""
+    @State var GoodThing4: String = ""
+    @State var GoodThing5: String = ""
+    @State var GoodThing6: String = ""
+    @State var GoodThing7: String = ""
+    var body: some View {
+        ScrollView {
+            Image("scientific research")
+            Text(GoodThingPrompt)
+                .font(.title)
+                .fixedSize(horizontal: false, vertical: true)
+            TextField("Hello", text: $GoodThing2)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hello", text: $GoodThing3)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hello", text: $GoodThing4)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hello", text: $GoodThing5)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hello", text: $GoodThing6)
+                .textFieldStyle(.roundedBorder)
+            TextField("Hello", text: $GoodThing7)
+                .textFieldStyle(.roundedBorder)
+        }
+    }
+}
